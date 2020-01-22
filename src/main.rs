540d616c36f2bb4f0b62eda33d6b6084ea88e4d4
hamza mahmood <hamzamahmood1124@gmail.com>
2020-01-22 15:27:36 -0800

    added

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+pub mod university{
+    pub mod study{
+       pub fn addstudent(){
+          println!{"students are important"}
+      }
+  }
+}
+fn main() {
+  university::study::addstudent();
+}
